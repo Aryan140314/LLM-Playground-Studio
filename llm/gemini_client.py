1f0daa134c3d68d9e9a5c51bf4e53e09f71d41cd
Aryan Singh <Aryanladdusingh@gmail.com>
2026-06-25 16:39:50 +0530

    Update the Logics

--- llm/gemini_client.py
+++ llm/gemini_client.py
@@ -1,15 +1,33 @@
+import time
 import google.generativeai as genai
 from config.api_keys import GEMINI_API_KEY
 
 
 class GeminiClient:
-    def __init__(self):
-        genai.configure(api_key=GEMINI_API_KEY)
-        self.model = genai.GenerativeModel("gemini-2.5-flash")
 
-    def generate_response(self, prompt: str):
-        try:
-            response = self.model.generate_content(prompt)
-            return response.text
-        except Exception as e:
-            return f"Error: {e}"
+    def __init__(self):
+
+        genai.configure(api_key=GEMINI_API_KEY)
+
+        self.model = genai.GenerativeModel(
+            "gemini-2.5-flash"
+        )
+
+    def generate_response(self, prompt):
+
+        start = time.time()
+
+        response = self.model.generate_content(prompt)
+
+        end = time.time()
+
+        return {
+
+            "text": response.text,
+
+            "response_time": round(end - start, 2),
+
+            "word_count": len(response.text.split()),
+
+            "character_count": len(response.text)
+        }
